aff4855b327ce9d5d06c6d36a80b5daf7c88a27b
redaaek <redabelhiaofficial@gmail.com>
2025-09-29 18:37:36 +0100

    Finalize Chrome extension with all features and fixes by adding Save and Delete buttons

--- script.js
+++ script.js
@@ -2,26 +2,52 @@ let myLeads = []
 const input = document.getElementsByTagName("input")[0]
 const btn = document.getElementsByTagName("button")[0]
 const ul = document.getElementsByTagName("ul")[0]
-btn.addEventListener("click", function () {
-    myLeads.push(input.value)
-    input.value = ""
-    renderLead()
+const savebtn=document.getElementsByTagName("button")[1]
+const rmvbtn = document.getElementsByTagName("button")[2]
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+
+savebtn.addEventListener("click" , function(){
+chrome.tabs.query({active:true , currentWindow: true}, 
+    function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
+    render(myLeads)
+
+})  
 })
-function renderLead() {
+function render(leads){      
     let listItems = ""
-    for (i = 0; i < myLeads.length; i++) {
-        // listItems += "<li><a href='"+ myLeads[i]+"'target='_blank'>" + myLeads[i] + "</a></li>"
+    for (i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-        <a href=https://${myLeads[i]} target="_blank">
-        ${myLeads[i]}
-        </a>
+             <a target='_blank' href='${leads[i]}'>
+                 ${leads[i]}
+             </a>
         </li>
         `
 
-        ul.innerHTML = listItems
     }
-
+    ul.innerHTML = listItems
 }
+
+
+rmvbtn.addEventListener("dblclick", function () {
+    localStorage.clear();
+    myLeads = []
+    render(myLeads)
+})
+
+btn.addEventListener("click", function () {
+    myLeads.push(input.value)
+    input.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
 
 
